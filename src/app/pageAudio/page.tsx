eabb3a33348d25ee905b02187b77dacb97ebbebb
Abhijeet <abhijeetcode05@gmail.com>
2024-02-28 07:48:47 +0530

    changes

--- src/app/pageAudio/page.tsx
+++ src/app/pageAudio/page.tsx
@@ -1,7 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
-// import { ArrowRight } from 'lucide-react'
-// import { buttonVariants } from '@/components/ui/button'
 import Image from "next/image";
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
@@ -9,6 +7,15 @@ import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <>
+      <Image src="/1.png" width={500} height={500} alt="audio" />
+      <Image
+      src="/dashboard-preview.jpg"
+          alt="product preview"
+          width={1364}
+          height={866}
+          quality={100}
+          className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+        />
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
 
         <h1>Audio Landing Page</h1>
@@ -22,6 +29,7 @@ export default function Home() {
         >
           Get started <ArrowRight className="ml-2 h-5 w-5" />
         </Link>
+        
       </MaxWidthWrapper>
     </>
   );
